88465e2efd755010572de148d7e07d889452746c
cheeseshadow <mifodii0@gmail.com>
2020-04-25 05:35:35 +0700

    add modals

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes/router'
 import VueRouter from 'vue-router'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(VModal)
 
 new Vue({
-  render: h => h(App),
-  router
+    render: h => h(App),
+    router
 }).$mount('#app')
