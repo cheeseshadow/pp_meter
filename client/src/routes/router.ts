e824ed658629137286bad5e5a94f1e897dd1fafb
cheeseshadow <mifodii0@gmail.com>
2020-04-26 09:06:24 +0700

    add redirect for non-existent routes

--- client/src/routes/router.ts
+++ client/src/routes/router.ts
@@ -3,21 +3,11 @@ import Login from '@/components/Login.vue'
 import Lobby from '@/components/Lobby.vue'
 
 const routes = [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    {name: 'lobby', path: '/lobby/:username', component: Lobby, props: true}
+    {path: '/login', component: Login},
+    {name: 'lobby', path: '/lobby/:username', component: Lobby, props: true},
+    {path: '*', redirect: '/login'}
 ]
 
-const router = new VueRouter({ mode: 'history', routes })
-
-router.beforeEach((to, from, next) => {
-
-    // console.log('to', to)
-    // console.log('from', from)
-
-    // console.log(document.cookie)
-
-    next()
-})
+const router = new VueRouter({mode: 'history', routes})
 
 export default router
